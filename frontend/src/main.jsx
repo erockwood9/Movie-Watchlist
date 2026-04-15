ed98bbe9a7bf56ab0b66c53f7d7b8ece07a879bc
erockwood9 <ethan.rawkwood@gmail.com>
2026-04-14 20:19:50 -0500

    Fix big where frontend doesn't render

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,16 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import { App } from './App.jsx'
-import { BrowserRouter } from 'react-router'
-import { Toaster } from "react-hot-toast"
-import './App.css'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { App } from "./App.jsx";
+import { BrowserRouter } from "react-router";
+import { Toaster } from "react-hot-toast";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter>
       <App />
       <Toaster />
     </BrowserRouter>
   </StrictMode>,
-)
+);
